84fb0191305209ccbea79f0797ef294dab96ba0d
Hussein Sawan <96922361+hu-sawan@users.noreply.github.com>
2022-08-31 18:32:05 +0300

    Main

--- main.js
+++ main.js
@@ -10,6 +10,55 @@ function getRepos() {
     if (input.value == "") {
         reposData.innerHTML = `<span>Please enter your github username</span>`
     } else {
+        fetch("https://api.github.com/users/hu-sawan/repos")
+        .then((response) => {
+            return response.json();
+        })
+        .then((repositories) => {
+            reposData.innerHTML = "";
 
+            // loop on repositories
+            repositories.forEach(repo => {
+
+                // create main div element
+                let mainDiv = document.createElement("div");
+
+                // create repo text
+                let repoName = document.createTextNode(repo.name);
+
+                mainDiv.appendChild(repoName);
+
+                // create repository url anchor tag
+                let url = document.createElement("a");
+                
+                // create repo url text
+                let urlText = document.createTextNode("Visit Repo");
+
+                url.appendChild(urlText);
+
+                // add link to a tag
+                url.href = `https://github.com/hu-sawan/${repo.name}`
+
+                // set target attribute
+                url.setAttribute("target", "_blank");
+
+                mainDiv.appendChild(url);
+
+                // create stars count
+                let starsSpan = document.createElement("span");
+
+                // create stars count text
+                let starsText = document.createTextNode(`Stars: ${repo.stargazers_count}`);
+
+                starsSpan.appendChild(starsText);
+
+                mainDiv.appendChild(starsSpan);
+
+                // add class for main div
+                mainDiv.className = "repo-box";
+
+                reposData.appendChild(mainDiv);
+            })
+        });
     }
 }

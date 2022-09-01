043b66a4cddda0806acf82617f2d2ea2c3ef450e
Hussein Sawan <96922361+hu-sawan@users.noreply.github.com>
2022-09-01 21:22:04 +0300

    Main

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function getRepos() {
                 // add class for main div
                 mainDiv.className = "repo-box";
 
-                reposData.appendChild(mainDiv);
+                reposData.appendChild(Hi);
             })
         });
     }
